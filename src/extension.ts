6272d42bb4291d3e64216fb63053185e452a89d6
李林积 <112566451+lilinji-TT@users.noreply.github.com>
2023-08-03 10:45:23 +0800

    fix: code refactor

--- src/extension.ts
+++ src/extension.ts
@@ -1,23 +1,15 @@
 import * as vscode from 'vscode'
-import { Editor } from './function/editor'
-const URL =
-  'http://s-gateway-qa.k8s.zhihuiya.com/s-ops-yaca-backend/2023-07/code/improvement'
+import { YACAPlugin, YACAPluginSelected } from './function/editor'
 export function activate(context: vscode.ExtensionContext) {
-  let disposable = vscode.commands.registerCommand(
+  let disposable1 = vscode.commands.registerCommand(
     'extension.YACA:VsCodePlugin',
-    Editor
+    YACAPlugin
   )
-
-  context.subscriptions.push(disposable)
-
   let disposable2 = vscode.commands.registerCommand(
-    'extension.myCustomCommandExplorer',
-    function () {
-      vscode.window.showInformationMessage(
-        'My Custom Command for Editor Executed!'
-      )
-    }
+    'extension.YACA:VsCodePluginSelected',
+    YACAPluginSelected
   )
+  context.subscriptions.push(disposable1)
   context.subscriptions.push(disposable2)
 }
 
--- src/type.ts
+++ src/type.ts
@@ -4,3 +4,21 @@ export enum ImprovementScope {
   DIRECTORY = 'DIRECTORY',
   PROJECT = 'PROJECT',
 }
+
+type Code = {
+  content: string
+  language: string
+}
+
+type ProjectObject = {
+  path: string
+  is_directory: boolean
+  size: number
+  code: Code
+}
+
+export type RequestData = {
+  scope: ImprovementScope
+  project_objects: ProjectObject[]
+  rules: string[]
+}
